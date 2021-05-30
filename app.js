e3172888d0d4c24b86e1945fae9c9af361717c9c
Giulia-Caldato <76591550+Giulia-Caldato@users.noreply.github.com>
2021-05-30 22:48:05 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const routes = {
+    '/login': { templateId: 'login' },
+    '/dashboard': { templateId: 'dashboard' },
+};
+
+function updateRoute() {
+    const path = window.location.pathname;
+    const route = routes[path];
+
+    if (!route) {
+      return navigate('/login');
+    }
+
+    const template = document.getElementById(route.templateId);
+    const view = template.content.cloneNode(true);
+    const app = document.getElementById('app');
+    app.innerHTML = '';
+    app.appendChild(view);
+}
+
+updateRoute('login');
+
+function navigate(path) {
+    window.history.pushState({}, path, window.location.origin + path);
+    updateRoute();
+}
+
+function onLinkClick(event) {
+    event.preventDefault();
+    navigate(event.target.href);
+}
+
+window.onpopstate = () => updateRoute();
+updateRoute();
